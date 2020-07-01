e8abb53bd5ebfcc54e3d2b9b2cdb67caa5161ea9
BonfaceKilz <bonfacemunyoki@gmail.com>
2020-07-01 23:40:03 +0300

    Wrap socket handle in a reader monad

--- src/Main.hs
+++ src/Main.hs
@@ -1,17 +1,42 @@
 module Main where
 
-import qualified Network.Socket as N
+import           Control.Exception
+import           Control.Monad.IO.Class
+import           Data.List
 import           System.IO
 import           System.Exit
-import           Data.List
+import qualified Network.Socket as N
+import           Control.Monad.Trans.Reader
 
 -- Config Options
 myServer = "irc.freenode.org" :: String
 myPort = 6667 :: N.PortNumber
-myChan = "#urbanperspective"
-myNick = "wednesday-dude-bot-test"
+myChan = "#urbanperspective" :: String
+myNick = "wednesday-dude-bot-test" :: String
+
+-- Main program entry
+main :: IO ()
+main = bracket connect disconnect loop
+  where
+    disconnect = hClose . botSocket
+    loop st    = runReaderT run st
+
+-- Net monad
+data Bot = Bot { botSocket :: Handle }
+type Net = ReaderT Bot IO
 
 -- Connect to a server given its home and port number
+connect :: IO Bot
+connect = notify $ do
+  h <- connectTo myServer myPort
+  return (Bot h)
+  where
+    notify a = bracket_
+      (putStrLn ("Connecting to " ++ myServer ++ " ...") >> hFlush stdout)
+      (putStrLn "done.")
+      a
+
+-- Connect to the server and return a handle
 connectTo :: N.HostName -> N.PortNumber -> IO Handle
 connectTo host port = do
   addr : _ <- N.getAddrInfo Nothing (Just host) (Just (show port))
@@ -19,22 +44,31 @@ connectTo host port = do
   N.connect sock (N.addrAddress addr)
   N.socketToHandle sock ReadWriteMode
 
+run :: Net ()
+run = do
+  write "NICK" myNick
+  write "USER" (myNick ++ " 0 * : tutorial bot")
+  write "JOIN" myChan
+  listen
+  
 -- Send a message to a handle
-write :: Handle -> String -> String -> IO ()
-write h cmd args = do
+write :: String -> String -> Net ()
+write cmd args = do
+  h <- asks botSocket
   let msg = cmd ++ " " ++ args ++ "\r\n"
-  hPutStr h msg
-  putStr (">" ++ msg)
+  liftIO $ hPutStr h msg
+  liftIO $ putStr ("> " ++ msg)
 
 --- Process each line from the server
-listen :: Handle -> IO ()
-listen h = forever $ do
-  line <- hGetLine h
-  putStrLn line
+listen :: Net ()
+listen = forever $ do
+  h <- asks botSocket
+  line <- liftIO $ hGetLine h
+  liftIO (putStrLn line)
   let s = init line
-  if isPing s then pong s else eval h (clean s)
+  if isPing s then pong s else eval (clean s)
   where
-    forever :: IO () -> IO ()
+    forever :: Net () -> Net ()
     forever a = do a; forever a
 
     clean :: String -> String
@@ -43,21 +77,13 @@ listen h = forever $ do
     isPing :: String -> Bool
     isPing x = "PING :" `isPrefixOf` x
 
-    pong :: String -> IO ()
-    pong x = write h "PONG" (':' : drop 6 x)
+    pong :: String -> Net ()
+    pong x = write "PONG" (':' : drop 6 x)
 
-eval :: Handle -> String -> IO ()
-eval h "!quit"                   = write h "QUIT" ":Exiting" >> exitSuccess
-eval h x | "!id " `isPrefixOf` x = privmsg h (drop 4 x)
-eval _ _                         = return ()
+eval :: String -> Net ()
+eval "!quit"                   = write "QUIT" ":Exiting" >> liftIO exitSuccess
+eval x | "!id " `isPrefixOf` x = privmsg (drop 4 x)
+eval _                         = return ()
 
-privmsg :: Handle -> String -> IO ()
-privmsg h s = write h "PRIVMSG" (myChan ++ " :" ++ s)
-
-main :: IO ()
-main = do
-  h <- connectTo myServer myPort
-  write h "NICK" myNick
-  write h "USER" (myNick ++ " 0 * :tutorial bot")
-  write h "JOIN" myChan
-  listen h
+privmsg :: String -> Net ()
+privmsg msg = write "PRIVMSG" (myChan ++ " :" ++ msg)
